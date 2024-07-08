2f43668c2457e40d732f6dac11d1e3ec1af654ea
lain <lyx@lyxincyberia.org>
2024-07-07 21:13:07 -0500

    checked long mode support

--- build.sh
+++ build.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-rm -f boot.bin boot.img
+rm -f boot.bin loader.bin os.bin
 
 nasm -f bin -o boot.bin boot.asm
 nasm -f bin -o loader.bin loader.asm
 
-# dd if=boot.bin of=boot.img bs=512 count=1
-# dd if=loader.bin of=boot.img bs=512 count=5 seek=1
 dd if=boot.bin >> os.bin
 dd if=loader.bin >> os.bin
 dd if=/dev/zero bs=512 count=5 >> os.bin
 
-qemu-system-x86_64 -hda os.bin
+qemu-system-x86_64 -hda os.bin -cpu host -enable-kvm
--- loader.asm
+++ loader.asm
@@ -2,6 +2,21 @@
 	[ORG 0x7e00]
 
 start:
+	mov [drive_id], dl
+
+	;; check maximum function of the cpu
+	mov eax, 0x80000000
+	cpuid
+	cmp eax, 0x80000001
+	jb long_mode_not_supported
+
+	mov eax, 0x80000001
+	cpuid
+	test edx, (1 << 29)
+	jz long_mode_not_supported
+	test edx, (1 << 26)
+	jz long_mode_not_supported
+
 	mov ah, 0x13
 	mov al, 1
 	mov bx, 0xa
@@ -10,9 +25,11 @@ start:
 	mov cx, message_len
 	int 0x10
 
+long_mode_not_supported:
 end:
 	hlt
 	jmp end
 
-message:	db "We are in the loader"
+drive_id:	db 0
+message:	db "long mode is supported"
 message_len:	equ $-message
